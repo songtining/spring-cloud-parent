51b817b2c0ed7e5cffa06d94c0ad104785ec5e02
宋体宁 <songtining@163.com>
2018-08-29 22:40:42 +0800

    test change

--- src/main/java/com/stn/springcloudparent/SpringCloudParentApplication.java
+++ src/main/java/com/stn/springcloudparent/SpringCloudParentApplication.java
@@ -3,6 +3,9 @@ package com.stn.springcloudparent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * This is the first SpringCloud demo project
+ */
 @SpringBootApplication
 public class SpringCloudParentApplication {
 
